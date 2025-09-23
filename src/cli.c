c27b38e2d830bb788e717903406fdf6033aca4a8
Syntax <abitacoolepic1@gmail.com>
2025-09-22 22:42:05 -0500

    fix: passed cli arguments being parsed as config

--- src/cli.c
+++ src/cli.c
@@ -25,26 +25,28 @@ cli_config_t cli_parse(int argc, char* argv[]) {
     config.flags = CONFIG_JIT | CONFIG_FILE;
 
     for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--verbose") == 0) {
-            config.flags |= CONFIG_VERBOSE;
-        } else if (strcmp(argv[i], "-j") == 0 || strcmp(argv[i], "--jit-off") == 0) {
-            config.flags &= ~CONFIG_JIT;
-        } else if (strcmp(argv[i], "-e") == 0 || strcmp(argv[i], "--execute") == 0) {
-            config.flags &= ~CONFIG_FILE;
-            if (i + 1 < argc) {
-                config.input = argv[++i];
+        if (!config.input) {
+            if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--verbose") == 0) {
+                config.flags |= CONFIG_VERBOSE;
+            } else if (strcmp(argv[i], "-j") == 0 || strcmp(argv[i], "--jit-off") == 0) {
+                config.flags &= ~CONFIG_JIT;
+            } else if (strcmp(argv[i], "-e") == 0 || strcmp(argv[i], "--execute") == 0) {
+                config.flags &= ~CONFIG_FILE;
+                if (i + 1 < argc) {
+                    config.input = argv[++i];
+                } else {
+                    panic("No string specified for -e");
+                    exit(1);
+                }
+            } else if (strcmp(argv[i], "-d") == 0 || strcmp(argv[i], "--debug") == 0) {
+                config.flags |= CONFIG_IR;
+            } else if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
+                cli_help();
+                exit(0);
             } else {
-                panic("No string specified for -e");
-                exit(1);
+                config.flags |= CONFIG_FILE;
+                config.input = argv[i];
             }
-        } else if (strcmp(argv[i], "-d") == 0 || strcmp(argv[i], "--debug") == 0) {
-            config.flags |= CONFIG_IR;
-        } else if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
-            cli_help();
-            exit(0);
-        } else if (!config.input) {
-            config.flags |= CONFIG_FILE;
-            config.input = argv[i];
         } else {
             config.args = argv + i;
             config.argc = argc - i;
