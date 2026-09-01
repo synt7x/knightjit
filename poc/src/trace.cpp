ecdd4fd9a63765a16f41d55fe68aa0b4890ba39e
unknown <abitacoolepic1@gmail.com>
2026-09-01 11:28:42 -0500

    poc: optimized machine code ADDs

--- poc/src/trace.cpp
+++ poc/src/trace.cpp
@@ -6,14 +6,48 @@
 
 using pool = uint64_t*;
 
+constexpr uint8_t STUB1[256] = {
+
+};
+
+constexpr uint8_t STUB2[256] = {
+
+};
+
+constexpr uint8_t ADD_INT[100] = {
+    // <GUARD>:
+    0x48, 0x89, 0xca, // mov rdx, rcx
+    0x48, 0x83, 0xe2, 0x07, // and rdx, 0x7
+    0x74, 0x06, // jz <SKIP>
+    0xff, 0x25, 0x00, 0x00, 0x00, 0x00, // jmp <STUB>
+    
+    // <SKIP>:
+    0x49, 0xff, 0x06, // inc qword [trace_slot_1]
+    0x48, 0x01, 0xcb, // add rbx, rcx
+};
+
+constexpr uint8_t ADD_STR[100] = {
+     // <GUARD>:
+    0x48, 0x89, 0xca, // mov rdx, rcx
+    0x48, 0x83, 0xe2, 0x07, // and rdx, 0x7
+    0x48, 0x83, 0xfa, 0x03, // cmp rdx, 3
+    0x74, 0x06, // jz <SKIP>
+    0xff, 0x25, 0x00, 0x00, 0x00, 0x00, // jmp <STUB>
+    
+    // <SKIP>:
+    0x49, 0xff, 0x46, 0x08, // inc qword [trace_slot_2]
+    0x48, 0x01, 0xcb, // add rbx, rcx
+};
+
 void tracer(pool trace_pool, void(*program)()) {
     uint8_t* code = reinterpret_cast<uint8_t*>(program);
     uint8_t patch = 0;
 
     uint8_t* target_address = code + 52;
-    void* stub = jalloc(256);
+    uint8_t* stub = reinterpret_cast<uint8_t*>(jalloc(256));
     jprotect(stub, 256);
 
+    std::cout << "[Tracer] Stub pool located " << (target_address - stub) << " bytes from the target address." << std::endl;
     std::cout << "[Tracer] Tracing thread started." << std::endl;
 
     while (true) {
